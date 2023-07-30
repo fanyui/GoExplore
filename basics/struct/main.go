0414fa06f0459f51a076a42256ee5b44499a4e5e
Harisu Fanyui <fanyuiharisu@gmail.com>
2023-07-30 10:49:28 +0100

    Feat: Add examples usage of struct

---
+++ basics/struct/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+type Person struct {
+	Name   string
+	Age    int
+	Height float64
+}
+func (c Person) String() string {
+	return  c.Name + " is " + strconv.Itoa(int(c.Age)) + " years old and " + strconv.FormatFloat(c.Height,'E', -1,32) + " feet tall"
+}
+func main()  {
+
+	person := Person{
+    Name:   "John Doe",
+    Age:    30,
+    Height: 5.8,
+	}
+	// Accessing struct fields
+	fmt.Println(person.Name)    // Outputs: John Doe
+	fmt.Println(person.Age)     // Outputs: 30
+	fmt.Println(person.Height)  // Outputs: 5.8
+
+	// Updating struct fields
+	person.Name = "Harisu Fanyui"
+	person.Age = 28
+	fmt.Println("Updated Name:", person.Name) // Outputs: Updated Name: Harisu Fanyui
+	fmt.Println("Updated Age:", person.Age)   // Outputs: Updated Age: 28
+	fmt.Println(person.String())
+}
